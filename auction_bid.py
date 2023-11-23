fe741b162be2602b25bd64c3c7b8ec376db2a75b
Syaugi Alkaf <125639879+SyaugiAlkaf@users.noreply.github.com>
2023-11-23 14:14:29 +0700

    Update auction_bid.py

--- auction_bid.py
+++ auction_bid.py
@@ -3,15 +3,18 @@ import json
 import threading
 import random
 import time
+import os
 from datetime import datetime
 
 class AuctionBidder:
-    def __init__(self, auction_id, bidder_id):
+    def __init__(self, auction_id, bidder_id, amqp_url):
         self.auction_id = auction_id
         self.bidder_id = bidder_id
+        self.amqp_url = amqp_url
 
     def place_bid(self):
-        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        params = pika.URLParameters(self.amqp_url)
+        connection = pika.BlockingConnection(params)
         channel = connection.channel()
 
         channel.exchange_declare(exchange='auction_events', exchange_type='fanout')
@@ -33,12 +36,15 @@ class AuctionBidder:
 if __name__ == '__main__':
     auction_id = "12345"
 
+    amqp_url = os.environ.get('albatross-01.rmq.cloudamqp.com',
+                              'amqps://zhmbpgxq:2IT7TpRnUaF62oQxjIcupAvAMxkuHvHo@albatross.rmq.cloudamqp.com/zhmbpgxq')
+
     bidders = [
-        AuctionBidder(auction_id, "Bidder_1"),
-        AuctionBidder(auction_id, "Bidder_2"),
-        AuctionBidder(auction_id, "Bidder_3"),
-        AuctionBidder(auction_id, "Bidder_4"),
-        AuctionBidder(auction_id, "Bidder_5"),
+        AuctionBidder(auction_id, "Bidder_1", amqp_url),
+        AuctionBidder(auction_id, "Bidder_2", amqp_url),
+        AuctionBidder(auction_id, "Bidder_3", amqp_url),
+        AuctionBidder(auction_id, "Bidder_4", amqp_url),
+        AuctionBidder(auction_id, "Bidder_5", amqp_url),
     ]
 
     for bidder in bidders:
