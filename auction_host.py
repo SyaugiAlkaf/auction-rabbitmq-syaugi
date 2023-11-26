be1c2c2630f06e44af53c0992ac21d9faf4f857b
Syaugi Alkaf <125639879+SyaugiAlkaf@users.noreply.github.com>
2023-11-26 19:26:52 +0700

    Update auction_host.py

--- auction_host.py
+++ auction_host.py
@@ -1,94 +1,174 @@
 import pika
 import json
 import time
-import os
 import operator
 import tkinter as tk
+from tkinter import messagebox
+import os
 
-class AuctionProvider:
-    def __init__(self, auction_id, starting_price, max_duration, amqp_url):
-        self.auction_id = auction_id
-        self.starting_price = starting_price
-        self.max_duration = max_duration
-        self.highest_bidder = None
-        self.highest_bid = starting_price
-        self.bidders = {}
+class AuctionApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Auction App")
+        self.root.geometry('300x400')
 
-        self.root = tk.Tk()
-        self.root.title(f"Auction Provider (Auction ID: {self.auction_id})")
+        self.auction_status_label = tk.Label(root, text="Auction Status: Not Running")
+        self.auction_status_label.pack()
 
-        self.current_bid_label = tk.Label(self.root, text="Current Bid: $")
-        self.winning_bid_label = tk.Label(self.root, text="Winning Bid: $")
-
+        self.current_bid_label = tk.Label(root, text="Current Bid: $")
         self.current_bid_label.pack()
+
+        self.winning_bid_label = tk.Label(root, text="Winning Bid: $")
         self.winning_bid_label.pack()
 
-        self.amqp_url = amqp_url
+        self.time_left_label = tk.Label(root, text="Time Left: ")
+        self.time_left_label.pack()
+
+        # Entry widgets for user input
+        self.label_nama = tk.Label(root, text="Nama Barang:")
+        self.label_nama.pack()
+        self.entry_nama = tk.Entry(root)
+        self.entry_nama.pack()
+
+        self.label_harga = tk.Label(root, text="Harga Barang (Rp):")
+        self.label_harga.pack()
+        self.entry_harga = tk.Entry(root)
+        self.entry_harga.pack()
+
+        self.label_waktu = tk.Label(root, text="Waktu Lelang (detik):")
+        self.label_waktu.pack()
+        self.entry_waktu = tk.Entry(root)
+        self.entry_waktu.pack()
+
+        # Button to start auction
+        self.start_button = tk.Button(root, text="Start Auction", command=self.start_auction)
+        self.start_button.pack()
+
+        self.auction_running = False
+        self.bidders = {}
+        self.highest_bidder = None
+        self.highest_bid = 0
+        self.auction_id = None
+        self.amqp_url = os.environ.get('albatross-01.rmq.cloudamqp.com',
+                                        'amqps://zhmbpgxq:2IT7TpRnUaF62oQxjIcupAvAMxkuHvHo@albatross.rmq.cloudamqp.com/zhmbpgxq')
+
+        self.root.after(1000, self.update_auction_status)
 
     def start_auction(self):
+        nama_barang = self.entry_nama.get()
+        harga_barang = self.entry_harga.get()
+        waktu_lelang = self.entry_waktu.get()
+
+        if not nama_barang or not harga_barang or not waktu_lelang:
+            messagebox.showerror("Error", "Please fill in all fields.")
+            return
+
+        self.auction_id = nama_barang
+        self.auction_running = True
+        self.auction_status_label.config(text="Auction Status: Running")
+
         params = pika.URLParameters(self.amqp_url)
         connection = pika.BlockingConnection(params)
-
         channel = connection.channel()
-
-        exchange_name = 'auction_events'
-        channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
+        channel.exchange_declare(exchange='auction_direct_exchange', exchange_type='direct')
 
         initial_auction_data = {
-            "auction_id": self.auction_id,
-            "starting_price": self.starting_price,
+            "auction_id": nama_barang,
+            "starting_price": int(harga_barang),
         }
-        channel.basic_publish(exchange=exchange_name, routing_key='', body=json.dumps(initial_auction_data))
-        print(f"Starting Auction (Auction ID: {self.auction_id})")
+        channel.basic_publish(exchange='auction_direct_exchange', routing_key=nama_barang, body=json.dumps(initial_auction_data))
+        print(f"Starting Auction (Auction ID: {nama_barang})")
 
-        result = channel.queue_declare(queue='', exclusive=True)
+        result = channel.queue_declare(queue=nama_barang, exclusive=True)
         queue_name = result.method.queue
-        channel.queue_bind(exchange=exchange_name, queue=queue_name)
+        channel.queue_bind(exchange='auction_direct_exchange', queue=queue_name, routing_key=nama_barang)
 
         def bid_callback(ch, method, properties, body):
             bid_data = json.loads(body)
-            bidder_id = bid_data['bidder_id']
-            bid_amount = bid_data['bid_amount']
-            self.bidders[bidder_id] = bid_amount
-            print(f"{bidder_id} placed a bid: ${bid_amount}")
+            bidder_id = bid_data.get('bidder_id')
+            bid_amount = bid_data.get('bid_amount')
 
-            self.current_bid_label.config(text=f"Current Bid: ${bid_amount}")
-            self.root.update()
+            if bidder_id and bid_amount:
+                self.bidders[bidder_id] = bid_amount
+                print(f"{bidder_id} placed a bid: ${bid_amount}")
+
+                # Check if the incoming bid is higher than the current highest bid
+                if bid_amount > self.highest_bid:
+                    self.highest_bid = bid_amount
+                    self.highest_bidder = bidder_id
+
+                self.current_bid_label.config(text=f"Current Bid: ${bid_amount}")
+                self.root.update()
+            else:
+                print("Invalid bid data received")
 
         channel.basic_consume(queue=queue_name, on_message_callback=bid_callback, auto_ack=True)
 
-        end_time = time.time() + self.max_duration
+        end_time = time.time() + float(waktu_lelang)
         while time.time() < end_time:
+            time_left = int(end_time - time.time())
+            self.send_highest_bid_time(nama_barang, harga_barang, end_time)
+            self.time_left_label.config(text=f"Time Left: {time_left} seconds")
+            self.root.update()
             connection.process_data_events()
 
         print("Auction Ended")
         self.display_winner_and_top_bids()
-
         connection.close()
-        self.root.mainloop()
+        self.auction_running = False
+        self.auction_status_label.config(text="Auction Status: Not Running")
+        self.time_left_label.config(text=f"Time Left: 0 seconds")
+
+    def send_highest_bid_time(self, nama_barang, harga_barang, end_time):
+        params = pika.URLParameters(self.amqp_url)
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+        channel.exchange_declare(exchange="host_exchange", exchange_type='fanout')
+
+        time_left = int(end_time - time.time())
+        if time_left < 0:
+            time_left = 0  # Ensure it's not negative
+
+        highest_bid_data = {
+            "auction_id": nama_barang,
+            "starting_bid": harga_barang,
+            "highest_bid": self.highest_bid,
+            "highest_bidder": self.highest_bidder,
+            "time_left_seconds": time_left
+        }
+        channel.basic_publish(
+            exchange="host_exchange",  # Separate exchange for host data
+            routing_key='',
+            body=json.dumps(highest_bid_data)
+        )
+        print(f"Data sent to Host's exchange [Auction_id: {nama_barang}, highest_bid: {self.highest_bid}, highest_bidder: {self.highest_bidder}, time_left_seconds: {time_left}]")
+        connection.close()
 
     def display_winner_and_top_bids(self):
         if self.bidders:
             sorted_bids = sorted(self.bidders.items(), key=operator.itemgetter(1), reverse=True)
-            winner_bidder, winning_bid = sorted_bids[0]
-            self.highest_bidder = winner_bidder
-            self.highest_bid = winning_bid
-            self.winning_bid_label.config(text=f"Winning Bid: ${winning_bid}")
+            self.highest_bidder, self.highest_bid = sorted_bids[0]
+            winner_label = tk.Label(self.root, text=f"Winning Bidder: {self.highest_bidder}")
+            winner_label.pack()
+            self.winning_bid_label.config(text=f"Winning Bid: ${self.highest_bid}")
 
-            print(f"Winner: {winner_bidder}, Winning Bid: ${winning_bid}")
+            print(f"Winner: {self.highest_bidder}, Winning Bid: ${self.highest_bid}")
             print("Top 5 Bids:")
             for i, (bidder, bid_amount) in enumerate(sorted_bids[:5], start=1):
                 print(f"{i}. {bidder}: ${bid_amount}")
         else:
             print("No bids received.")
 
-if __name__ == '__main__':
-    auction_id = "12345"
-    starting_price = 100
-    max_duration = 30
+    def update_auction_status(self):
+        if self.auction_running:
+            self.auction_status_label.config(text="Auction Status: Running")
+        else:
+            self.auction_status_label.config(text="Auction Status: Not Running")
 
-    amqp_url = os.environ.get('albatross-01.rmq.cloudamqp.com',
-                              'amqps://zhmbpgxq:2IT7TpRnUaF62oQxjIcupAvAMxkuHvHo@albatross.rmq.cloudamqp.com/zhmbpgxq')
+        self.root.after(1000, self.update_auction_status)
 
-    provider = AuctionProvider(auction_id, starting_price, max_duration, amqp_url)
-    provider.start_auction()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AuctionApp(root)
+    root.mainloop()
